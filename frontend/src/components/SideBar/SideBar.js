9e4e94084f8a5baf5ed7b6eb51d566bccb1dd33a
Michael Dür <62062370+michael-duer@users.noreply.github.com>
2024-03-20 10:36:45 +0100

    Add routing and update component naming
    - Implement routing with react-router-dom
    - Add a new tab in SideBar.js for the viewer component introduced in the previous commit.
    - Rename ViewerContainer to MetaInformationContainer to improve clarity.

--- frontend/src/components/SideBar/SideBar.js
+++ frontend/src/components/SideBar/SideBar.js
@@ -1,51 +1,54 @@
-import { drawerWidth } from '../utils';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import { drawerWidth } from "../utils";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import {
   ListItemButton,
   ListItemIcon,
   ListItemText,
   Toolbar,
-} from '@mui/material';
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import { styled } from '@mui/material/styles';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import MuiDrawer from '@mui/material/Drawer';
+} from "@mui/material";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import ViewInArIcon from "@mui/icons-material/ViewInAr";
+import { styled } from "@mui/material/styles";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import MuiDrawer from "@mui/material/Drawer";
+import { useNavigate } from "react-router-dom";
 
 const Drawer = styled(MuiDrawer, {
-  shouldForwardProp: (prop) => prop !== 'open',
+  shouldForwardProp: (prop) => prop !== "open",
 })(({ theme, open }) => ({
-  '& .MuiDrawer-paper': {
-    position: 'relative',
-    whiteSpace: 'nowrap',
+  "& .MuiDrawer-paper": {
+    position: "relative",
+    whiteSpace: "nowrap",
     width: drawerWidth,
-    transition: theme.transitions.create('width', {
+    transition: theme.transitions.create("width", {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.enteringScreen,
     }),
-    boxSizing: 'border-box',
+    boxSizing: "border-box",
     ...(!open && {
-      overflowX: 'hidden',
-      transition: theme.transitions.create('width', {
+      overflowX: "hidden",
+      transition: theme.transitions.create("width", {
         easing: theme.transitions.easing.sharp,
         duration: theme.transitions.duration.leavingScreen,
       }),
       width: theme.spacing(7),
-      [theme.breakpoints.up('sm')]: {
+      [theme.breakpoints.up("sm")]: {
         width: theme.spacing(9),
       },
     }),
   },
 }));
 const SideBar = ({ open, toggleDrawer }) => {
+  const navigate = useNavigate();
   return (
-    <Drawer variant='permanent' open={open}>
+    <Drawer variant="permanent" open={open}>
       <Toolbar
         sx={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'flex-end',
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "flex-end",
           px: [1],
         }}
       >
@@ -54,12 +57,18 @@ const SideBar = ({ open, toggleDrawer }) => {
         </IconButton>
       </Toolbar>
       <Divider />
-      <List component='nav'>
-        <ListItemButton>
+      <List component="nav">
+        <ListItemButton onClick={() => navigate("/")}>
           <ListItemIcon>
             <DashboardIcon />
           </ListItemIcon>
-          <ListItemText primary='Dashboard' />
+          <ListItemText primary="Dashboard" />
+        </ListItemButton>
+        <ListItemButton onClick={() => navigate("/image-viewer")}>
+          <ListItemIcon>
+            <ViewInArIcon />
+          </ListItemIcon>
+          <ListItemText primary="Viewer" />
         </ListItemButton>
       </List>
     </Drawer>
