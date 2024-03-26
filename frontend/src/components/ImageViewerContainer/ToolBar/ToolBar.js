9bb2720b708ede83866142753444982aad39c0ea
Laura Bünemann <laura.buenemann@hotmail.com>
2024-03-26 08:51:40 +0100

    Center Icon

--- frontend/src/components/ImageViewerContainer/ToolBar/ToolBar.js
+++ frontend/src/components/ImageViewerContainer/ToolBar/ToolBar.js
@@ -4,18 +4,24 @@ import BrushIcon from '@mui/icons-material/Brush';
 import StraightenIcon from '@mui/icons-material/Straighten';
 import VerticalSplitIcon from '@mui/icons-material/VerticalSplit';
 
+const iconStyle = {
+  marginRight: '0px', // Centers the icon, as it takes away the space for the text expected by the mui-toolbar
+};
 
 const ToolBar = ({ onPaintClick, onMeasureClick, onScrollClick }) => {
-    return (
-      <MuiToolbar>
-        <Button variant="contained" color='inherit' startIcon={<BrushIcon />} onClick={onPaintClick}>
-        </Button>
-        <Button variant="contained" color='inherit' startIcon={<StraightenIcon />} onClick={onMeasureClick}>
-        </Button>
-        <Button variant="contained" color="inherit" startIcon={<VerticalSplitIcon />} onClick={onScrollClick}>
-        </Button>
-      </MuiToolbar>
-    );
-  };
+  return (
+    <MuiToolbar>
+      <Button variant="contained" color='inherit' onClick={onPaintClick}>
+        <BrushIcon style={iconStyle} />
+      </Button>
+      <Button variant="contained" color='inherit' onClick={onMeasureClick}>
+        <StraightenIcon style={iconStyle} />
+      </Button>
+      <Button variant="contained" color="inherit" onClick={onScrollClick}> 
+        <VerticalSplitIcon style={iconStyle} /> 
+      </Button>
+    </MuiToolbar>
+  );
+};
 
 export default ToolBar;
